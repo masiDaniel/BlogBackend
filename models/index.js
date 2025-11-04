b203151cd06ad585af236e769d5fd3c7e12a6142
masiDaniel <masidaniel02@gmail.com>
2025-11-04 11:20:54 +0300

    chore(functionality): added the relevant imports to ensure seamles workflow

--- models/index.js
+++ models/index.js
@@ -1,4 +1,6 @@
 const User = require("./User");
 const Category = require("./Category");
+const File = require("./file");
+const Post = require("./Post")
 
-module.exports = {User, Category};
+module.exports = {User, Category, File, Post};
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 const authRoute = require("./auth");
 const categoryRoute = require("./category");
-const fileRoute = require("./file")
+const fileRoute = require("./file");
+const postRoute = require("./post");
 
-module.exports = {authRoute, categoryRoute, fileRoute}
+
+module.exports = {authRoute, categoryRoute, fileRoute, postRoute}
